2a8323f110350f5b8be303f280c5fc59452e8cbb
NallabellyVinay <52525335+NallabellyVinay@users.noreply.github.com>
2024-07-30 00:08:21 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -5,38 +5,47 @@ class BatteryMonitor:
         self.soc_warning_tolerance = 80 * 0.05
         self.charge_rate_warning_tolerance = 0.8 * 0.05
 
+    def report_temperature_warnings(self, temperature):
+        if 0 <= temperature < 0 + self.temperature_warning_tolerance:
+            self.reporter('Warning: Temperature approaching low limit')
+        if 45 - self.temperature_warning_tolerance < temperature <= 45:
+            self.reporter('Warning: Temperature approaching high limit')
+
+    def report_soc_warnings(self, soc):
+        if 20 <= soc < 20 + self.soc_warning_tolerance:
+            self.reporter('Warning: Approaching discharge')
+        if 80 - self.soc_warning_tolerance < soc <= 80:
+            self.reporter('Warning: Approaching charge-peak')
+
+    def report_charge_rate_warnings(self, charge_rate):
+        if 0.8 - self.charge_rate_warning_tolerance < charge_rate <= 0.8:
+            self.reporter('Warning: Charge rate approaching high limit')
+
     def check_temperature(self, temperature):
         if temperature < 0:
             self.reporter('Temperature too low!')
             return False
-        if temperature >= 0 and temperature < 0 + self.temperature_warning_tolerance:
-            self.reporter('Warning: Temperature approaching low limit')
         if temperature > 45:
             self.reporter('Temperature too high!')
             return False
-        if temperature > 45 - self.temperature_warning_tolerance:
-            self.reporter('Warning: Temperature approaching high limit')
+        self.report_temperature_warnings(temperature)
         return True
 
     def check_soc(self, soc):
         if soc < 20:
             self.reporter('State of Charge too low!')
             return False
-        if soc >= 20 and soc < 20 + self.soc_warning_tolerance:
-            self.reporter('Warning: Approaching discharge')
         if soc > 80:
             self.reporter('State of Charge too high!')
             return False
-        if soc > 80 - self.soc_warning_tolerance:
-            self.reporter('Warning: Approaching charge-peak')
+        self.report_soc_warnings(soc)
         return True
 
     def check_charge_rate(self, charge_rate):
         if charge_rate > 0.8:
             self.reporter('Charge rate too high!')
             return False
-        if charge_rate > 0.8 - self.charge_rate_warning_tolerance:
-            self.reporter('Warning: Charge rate approaching high limit')
+        self.report_charge_rate_warnings(charge_rate)
         return True
 
     def battery_is_ok(self, temperature, soc, charge_rate):
